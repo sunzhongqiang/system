f722290d77ed22819c6024d54e5da21b91c3dd50
孙中强 <Administrator@PC201506111414>
2016-10-28 15:52:07 +0800

    接口

--- src/main/java/com/mmk/api/WxUserApi.java
+++ src/main/java/com/mmk/api/WxUserApi.java
@@ -4,7 +4,25 @@ import com.mmk.business.model.WxUser;
 import com.mmk.common.model.ResultMsg;
 
 public interface WxUserApi {
-	
+	/**
+	 * 用户保存
+	 * @param user
+	 * @return
+	 */
 	ResultMsg save(WxUser user);
+
+	/**
+	 * 用户登录
+	 * @param user 要登录的用户
+	 * @return
+	 */
+	ResultMsg login(WxUser user);
+	
+	/**
+	 * 根据用户openid获取用户
+	 * @param openid
+	 * @return
+	 */
+	WxUser findUser(String openid);
 
 }
--- src/main/java/com/mmk/api/impl/WxUserApiImpl.java
+++ src/main/java/com/mmk/api/impl/WxUserApiImpl.java
@@ -28,5 +28,24 @@ public class WxUserApiImpl implements WxUserApi {
 		}
 		return new ResultMsg(false,"用户已经存在！");
 	}
+	
+	@RequestMapping("/api/user/login")
+	@Override
+	public ResultMsg login(WxUser user) {
+		WxUser wxUser = wxUserService.findBy("openid", user.getOpenid());
+		if(wxUser==null){
+			//新增用户逻辑
+			wxUserService.save(user);
+			return new ResultMsg(true, "用户新增成功");
+			
+		}
+		return new ResultMsg(false,"用户已经存在！");
+	}
+
+	@RequestMapping("/api/user/findUser")
+	@Override
+	public WxUser findUser(String openid) {
+		return wxUserService.findBy("openid", openid);
+	}
 
 }
