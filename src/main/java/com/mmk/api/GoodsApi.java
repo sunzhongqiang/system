0563e9632caf02860bfbb74562b923096eb7e0db
胡广玲 <huguangling@PC201506111414>
2016-10-31 11:30:49 +0800

    商品接口

---
+++ src/main/java/com/mmk/api/GoodsApi.java
@@ -0,0 +1,23 @@
+package com.mmk.api;
+
+import com.mmk.business.model.Goods;
+import com.mmk.business.model.WxUser;
+import com.mmk.common.model.ResultMsg;
+
+public interface GoodsApi {
+	/**
+	 * 商品保存
+	 * @param user
+	 * @return
+	 */
+	ResultMsg save(Goods goods);
+
+	/**
+	 * 商品检索
+	 * @param goods 要检索商品的Id
+	 * @return
+	 */
+	ResultMsg findGoods(String id);
+	
+
+}
---
+++ src/main/java/com/mmk/api/impl/GoodsApiImpl.java
@@ -0,0 +1,66 @@
+package com.mmk.api.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mmk.api.GoodsApi;
+import com.mmk.business.model.Goods;
+import com.mmk.business.model.WxUser;
+import com.mmk.business.service.WxUserService;
+import com.mmk.business.service.impl.GoodsServiceImpl;
+import com.mmk.common.model.ResultMsg;
+
+@RestController
+public class GoodsApiImpl implements GoodsApi{
+	
+	@Resource
+	private GoodsServiceImpl goodsServiceImpl;
+	
+	
+	@RequestMapping("/api/goods/save")
+	@Override
+	public ResultMsg save(Goods good) {
+		Goods goods = goodsServiceImpl.findBy("openid", good.getId());
+		if (goods == null) {
+			// 新增商品逻辑
+			goodsServiceImpl.save(good);
+			return new ResultMsg(true, "商品新增成功");
+
+		} else {
+			goods.setGoodsCat(good.getGoodsCat());
+			goods.setGoodsMainImg(good.getGoodsMainImg());
+			goods.setGoodsName(good.getGoodsName());
+			goods.setGoodsNumber(good.getGoodsNumber());
+			goods.setGoodsOriginalImg(good.getGoodsOriginalImg());
+			goods.setGoodsOriginalPrice(good.getGoodsOriginalPrice());
+			goods.setGoodsThumb(good.getGoodsThumb());
+			goods.setIsDelete(good.getIsDelete());
+			goods.setPromoteEndDate(good.getPromoteEndDate());
+			goods.setPromoteNumber(goods.getPromoteNumber());
+			goods.setPromotePrice(good.getPromotePrice());
+			goods.setPromoteStartDate(good.getPromoteStartDate());
+			goods.setGoodsOriginalPrice(good.getGoodsOriginalPrice());
+		}
+		return new ResultMsg(false, "商品已经存在！");
+	}
+
+
+	@RequestMapping("/api/goods/findGoods")
+	@Override
+	public ResultMsg findGoods(String id) {
+		Goods good = goodsServiceImpl.findBy("id", id);
+		if (good == null) {
+			return new ResultMsg(false, "该商品不存在");
+		} else {
+			Map<String, Object> result = new HashMap<String, Object>();
+			result.put("good", good);
+			return new ResultMsg(true, "查找成功", result);
+		}
+	}
+
+}
