77f299588a3d47e68830babb13c5757e0537e23d
xuzhihao <xuzhihao@PC201506111525>
2016-11-03 14:19:21 +0800

    空判断

--- src/main/java/com/mmk/common/model/EasyPageable.java
+++ src/main/java/com/mmk/common/model/EasyPageable.java
@@ -14,6 +14,11 @@ public class EasyPageable {
 	private String sort;
 	private String order;
 	
+	public EasyPageable() {
+		this.page =1 ;
+		this.rows = 50;
+	}
+	
 	
 	
 	public Integer getPage() {
@@ -44,11 +49,21 @@ public class EasyPageable {
 		this.order = order;
 	}
 	public Pageable pageable(){
+		checkNull();
 		if(StringUtils.isNoneBlank(sort)){
 			Direction dirction = "desc".equals(order) ? Direction.DESC :Direction.ASC;
 			return new PageRequest(page-1, rows,new Sort(dirction,sort ));
 		}
 	    return new PageRequest(page-1, rows);
+	}
+	
+	private void checkNull(){
+		if(this.page==null){
+			this.page = 1;
+		} 
+		if(this.rows == null){
+			this.rows = 50;
+		}
 	}
 	
 	
