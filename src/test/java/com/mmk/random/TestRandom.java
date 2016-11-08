c53868323735993e0df920561e099404a4a0ccd6
孙中强 <Administrator@PC201506111414>
2016-11-08 13:50:27 +0800

    随机数测试

---
+++ src/test/java/com/mmk/random/TestRandom.java
@@ -0,0 +1,75 @@
+package com.mmk.random;
+
+import java.util.Random;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class TestRandom {
+	
+	private static Log log = LogFactory.getLog(TestRandom.class);
+
+	public static void main(String[] args) {
+			Random random = new Random();
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+			log.info(random.nextInt(10));
+		}
+
+
+}
