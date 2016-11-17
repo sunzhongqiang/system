8a01f8916b2dd7158e0606f551754000e093715f
xuzhihao <xuzhihao@PC201506111525>
2016-11-17 16:33:50 +0800

    修改

--- src/main/resources/static/js/tuan/list.js
+++ src/main/resources/static/js/tuan/list.js
@@ -96,6 +96,7 @@
             rownumbers : true,
             pagination : false,
             singleSelect : true,
+            nowrap :false,
             idField : 'id',
             pageSize : 50,
             pageList : [ 10, 20, 30, 40, 50, 100, 200, 300, 400, 500 ],
