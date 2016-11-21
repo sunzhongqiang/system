4f678a1e881dfb2757aec1035967874492d0e050
孙中强 sunzhongqiang <aboysun@126.com>
2016-11-21 14:47:54 +0800

    商品销售属性

--- src/main/resources/static/js/goods/form.js
+++ src/main/resources/static/js/goods/form.js
@@ -1,115 +1,51 @@
- var dataGrid;
+//代码生成工具自动生成，请在此处填写 查询页面使用的js代码
+    var dataGrid;
     $(function() {
         dataGrid = $('#dataGrid').datagrid({
-            url : '/goods/gridData',
+            url : '/goodsSku/gridData',
             fit : true,
-            fitColumns : true,
             striped : true,
             rownumbers : true,
             pagination : true,
             singleSelect : true,
-            nowrap : false,
             idField : 'id',
             pageSize : 50,
             pageList : [ 10, 20, 30, 40, 50, 100, 200, 300, 400, 500 ],
             columns : [ [ 
                     {
                 width : '80',
-                title : '商品ID',
+                title : '商品属性ID',
                 field : 'id',
-                align: 'center',
             },
                     {
                 width : '80',
-                title : '商品分类',
-                field : 'goodsCat',
-                align: 'center',
-                formatter : function(value, row, index) {
-					switch (value) {
-					case 0:
-						return '一元购';
-					case 1:
-						return '拼团';
-					}
-				}
-                
-            },
-//            		{
-//                width : '80',
-//                title : '商品主图',
-//                field : 'goodsMainImg',
-//                formatter:function(value,row,index)
-//                {return '<img src='+value+'>';}
-//            },
-                    {
-                width : '200',
-                title : '商品名称',
-                field : 'goodsName',
+                title : '商品id',
+                field : 'goodId',
             },
                     {
                 width : '80',
-                title : '商品数量',
-                field : 'goodsNumber',
-                align: 'center',
+                title : '属性值',
+                field : 'skuName',
             },
                     {
                 width : '80',
-                title : '商品原价',
-                field : 'goodsOriginalPrice',
-                align: 'center',
+                title : '商品价格',
+                field : 'price',
             },
                     {
                 width : '80',
-                title : '团购价',
-                field : 'promotePrice',
-                align: 'center',
-            },
-                    {
-                width : '150',
-                title : '促销开始时间',
-                field : 'promoteStartDate',
-                align: 'center',
-            },
-                    {
-                width : '150',
-                title : '促销结束时间',
-                field : 'promoteEndDate',
-                align: 'center',
+                title : '该sku库存',
+                field : 'stock',
             },
                     {
                 width : '80',
-                title : '促销数量',
-                field : 'promoteNumber',
-                align: 'center',
+                title : '商品条形码',
+                field : 'code',
             },
-//                    {
-//                width : '80',
-//                title : '已售数量',
-//                field : 'saledNumber',
-//            },
-//                    {
-//                width : '80',
-//                title : '商品相册',
-//                field : 'goodsThumb',
-//            },
-//                    {
-//                width : '80',
-//                title : '商品原图',
-//                field : 'goodsOriginalImg',
-//            },
                     {
                 width : '80',
-                title : '商品是否下架',
-                field : 'isDelete',
-                align: 'center',
-                formatter : function(value, row, index) {
-					switch (value) {
-					case 0:
-						return '上架';
-					case 1:
-						return '下架';
-					}
-				}
+                title : '商品重量',
+                field : 'weight',
             },
             {
                 field : 'action',
@@ -127,9 +63,7 @@
            toolbar :  [{
 	            iconCls: 'icon-add',
 	            text:'添加销售属性',
-	            handler: function(){
-	            	addFun();
-	            }
+	            handler: function(){addFun();}
             }],
             onLoadSuccess : function(data){
                 $('.btn_edit').linkbutton({text:'编辑',plain:true,iconCls:'icon-edit'});
@@ -163,10 +97,10 @@
         } else {//点击操作里面的删除图标会触发这个
             dataGrid.datagrid('unselectAll').datagrid('uncheckAll');
         }
-        parent.$.messager.confirm('询问', '您是否要删除商品活动？', function(b) {
+        parent.$.messager.confirm('询问', '您是否要删除商品SKU？', function(b) {
             if (b) {
                 progressLoad();
-                    $.post('/goods/delete', {
+                    $.post('/goodsSku/delete', {
                         id : id
                     }, function(result) {
                         if (result.success) {
@@ -186,24 +120,31 @@
         } else {
             dataGrid.datagrid('unselectAll').datagrid('uncheckAll');
         }
-        
-        parent.addTab({
-			url : '/goods/edit?id=' + id,
-			title : '修改商品',
-		},true);
-//        parent.$.modalDialog({
-//            title : '编辑',
-//            width : 500,
-//            height : 300,
-//            href : '/goods/edit?id=' + id,
-//            buttons : [ {
-//                text : '编辑',
-//                handler : function() {
-//                    parent.$.modalDialog.openner_dataGrid = dataGrid;//因为添加成功之后，需要刷新这个dataGrid，所以先预定义好
-//                    var f = parent.$.modalDialog.handler.find('#modelForm');
-//                    f.submit();
-//                }
-//            } 
-//            ]
-//        });
+        parent.$.modalDialog({
+            title : '编辑',
+            width : 500,
+            height : 300,
+            href : '/goodsSku/edit?id=' + id,
+            buttons : [ {
+                text : '编辑',
+                handler : function() {
+                    parent.$.modalDialog.openner_dataGrid = dataGrid;//因为添加成功之后，需要刷新这个dataGrid，所以先预定义好
+                    var f = parent.$.modalDialog.handler.find('#modelForm');
+                    f.submit();
+                }
+            } 
+            ]
+        });
     }
+    
+    function searchFun() {
+        ////将searchForm表单内的元素序列为对象传递到后台
+        dataGrid.datagrid('load', $.serializeObject($('#searchForm')));
+    }
+    function cleanFun() {
+        //找到form表单下的所有input标签并清空
+        $('#searchForm input').val('');
+        //重新加载数据，无填写数据，向后台传递值则为空
+        dataGrid.datagrid('load', {});
+    }
+    
