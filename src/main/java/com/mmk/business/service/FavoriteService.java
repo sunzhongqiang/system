6c8ff70334b8ca67bc5f9fd91aed06d433ce7c79
胡广玲 <huguangling@PC201506111414>
2016-11-30 09:25:00 +0800

    团收藏

---
+++ src/main/java/com/mmk/business/service/FavoriteService.java
@@ -0,0 +1,97 @@
+package com.mmk.business.service;
+
+import java.util.List;
+import com.mmk.gene.service.BaseService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.mmk.business.model.Favorite;
+import com.mmk.business.condition.FavoriteCondition;
+
+/**
+* FavoriteService: 团收藏 业务服务层接口
+*2016-11-30 09:24:22
+*@author huguangling 胡广玲
+*@version 1.0
+*/
+public interface FavoriteService extends BaseService<Favorite, Long> {
+    /**
+     * 生成的列表分页查询方法
+     * @param favoriteCondition  查询条件
+     * @param pageable 分页参数
+     * @return 分页返回查询的结果
+     * @author huguangling 胡广玲
+     * 
+     */
+    Page<Favorite> list(FavoriteCondition favoriteCondition, Pageable pageable);
+    
+    /**
+     * 不分页查询相关信息，根据传入的model类对象取得查询结果集List
+     * @param  favorite 查询类
+     * @return 查询的结果集
+     * @author huguangling 胡广玲
+     * 
+     */
+    public List<Favorite> list(FavoriteCondition favorite);
+
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param id 收藏主键
+     * @return 符合条件的唯一对象
+     * @author huguangling 胡广玲
+     * 
+     */
+    Favorite findById(Long id);
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param groupId 团id
+     * @return 符合条件的唯一对象
+     * @author huguangling 胡广玲
+     * 
+     */
+    Favorite findByGroupId(Long groupId);
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param goodsId 商品主键
+     * @return 符合条件的唯一对象
+     * @author huguangling 胡广玲
+     * 
+     */
+    Favorite findByGoodsId(Long goodsId);
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param userId 用户ID
+     * @return 符合条件的唯一对象
+     * @author huguangling 胡广玲
+     * 
+     */
+    Favorite findByUserId(Long userId);
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param openid openid
+     * @return 符合条件的唯一对象
+     * @author huguangling 胡广玲
+     * 
+     */
+    Favorite findByOpenid(String openid);
+    /**
+     * 根据给定的字段和属性值，获得符合条件的第一个结果
+     * @param field Favorite 中的某个字段
+     * @param value 字段的值
+     * @return Favorite 返回符合条件的结果，如果没有返回null
+     * @author huguangling 胡广玲
+     * 
+     * 
+     */
+    Favorite findBy(String field,Object value);
+    
+    /**
+     * 根据给定的字段和属性值，获得符合条件的所有结果
+     * @param field Favorite中的某个字段
+     * @param value 字段的值
+     * @return 返回符合条件的所有结果
+     * @author huguangling 胡广玲
+     * 
+     * 
+     */
+    List<Favorite> findAllBy(String field,Object value);
+}
---
+++ src/main/java/com/mmk/business/service/impl/FavoriteServiceImpl.java
@@ -0,0 +1,109 @@
+package com.mmk.business.service.impl;
+
+import javax.annotation.Resource;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import com.mmk.gene.service.impl.BaseServiceImpl;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.mmk.business.dao.FavoriteRepository;
+import com.mmk.business.model.Favorite;
+import com.mmk.business.condition.FavoriteCondition;
+import com.mmk.business.service.FavoriteService;
+import com.mmk.business.dao.FavoriteDao;
+/**
+* FavoriteServiceImpl: 团收藏 业务服务层实现
+* 2016-11-30 09:24:22
+* @author huguangling 胡广玲
+* @version 1.0
+*/
+@Service
+public class FavoriteServiceImpl extends BaseServiceImpl<Favorite, Long> implements FavoriteService {
+
+    private Log log = LogFactory.getLog(this.getClass());
+    @Resource
+    private FavoriteDao favoriteDao;
+    
+    private FavoriteRepository favoriteRepository;
+    /**
+    *构造方法
+    * @param favoriteRepository 数据容器
+    */
+    @Autowired
+    public FavoriteServiceImpl( FavoriteRepository favoriteRepository) {
+        super(favoriteRepository);
+        this.favoriteRepository = favoriteRepository;
+    }
+
+    @Override
+    public Page<Favorite> list(FavoriteCondition favoriteCondition, Pageable pageable) {
+        log.info("团收藏查询列表");
+        return favoriteDao.list(favoriteCondition, pageable);
+    }
+    
+    @Override
+    public List<Favorite> list(FavoriteCondition favoriteCondition) {
+        log.info("团收藏查询列表无分页");
+        return favoriteDao.list(favoriteCondition);
+    }
+
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param id 收藏主键
+     * @return 符合条件的唯一对象
+     */
+    @Override
+    public Favorite findById(Long id){
+         return favoriteRepository.findFirstById(id);
+    }
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param groupId 团id
+     * @return 符合条件的唯一对象
+     */
+    @Override
+    public Favorite findByGroupId(Long groupId){
+         return favoriteRepository.findFirstByGroupId(groupId);
+    }
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param goodsId 商品主键
+     * @return 符合条件的唯一对象
+     */
+    @Override
+    public Favorite findByGoodsId(Long goodsId){
+         return favoriteRepository.findFirstByGoodsId(goodsId);
+    }
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param userId 用户ID
+     * @return 符合条件的唯一对象
+     */
+    @Override
+    public Favorite findByUserId(Long userId){
+         return favoriteRepository.findFirstByUserId(userId);
+    }
+    /**
+     * 根据给定的字段返回符合的对象
+     * @param openid openid
+     * @return 符合条件的唯一对象
+     */
+    @Override
+    public Favorite findByOpenid(String openid){
+         return favoriteRepository.findFirstByOpenid(openid);
+    }
+    @Override 
+    public Favorite findBy(String field,Object value){
+        log.info("团收藏根据字["+field+"="+value+"] 进行查询符合条件的唯一值");
+        return favoriteDao.findBy(field,value);
+    }
+    
+    @Override 
+    public List<Favorite> findAllBy(String field,Object value){
+        log.info("团收藏根据字["+field+"="+value+"] 进行查询符合条件的所有记录");
+        return favoriteDao.findAllBy(field,value);
+    }
+}
