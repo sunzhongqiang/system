f8dabd81d285fd28817402665f7f82cd6176ea26
胡广玲 <huguangling@PC201506111414>
2016-11-30 09:25:57 +0800

    商品或者团的关注

---
+++ src/main/java/com/mmk/business/condition/AttentionCondition.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ *  AttentionCondition 创建于 2016-11-30 09:25:29 版权归作者和作者当前组织所有
+ */
+package com.mmk.business.condition;
+import java.util.Date;
+import com.mmk.business.model.Attention;
+
+/**
+* AttentionCondition ： 商品或者团的关注 扩展查询模型
+* 2016-11-30 09:25:29
+*@author huguangling 胡广玲
+*@version 1.0
+*
+*/
+public class AttentionCondition extends Attention{
+
+
+
+}
