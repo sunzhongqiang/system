3b933e819efbf3b33c3116e45fea3a7ca45d6d2c
孙中强 sunzhongqiang <aboysun@126.com>
2016-12-08 15:48:10 +0800

    去除多余的model

--- src/main/java/com/mmk/weixin/constants/WeiXinOpenParams.java
+++ src/main/java/com/mmk/weixin/constants/WeiXinOpenParams.java
@@ -6,7 +6,7 @@ public class WeiXinOpenParams {
 	 * 微信预授权参数
 	 */
 	public static final String AUTHORIZATION_URL = "https://mp.weixin.qq.com/cgi-bin/componentloginpage";
-	public static final String COMPONENT_APPID = "wx8dc8c585221427ff" ;
+	public static final String COMPONENT_APPID = "wx8dc	8c585221427ff" ;
 	public static final String PRE_AUTH_CODE = "";
 	public static final String REDIRECT_URI = "";
 
