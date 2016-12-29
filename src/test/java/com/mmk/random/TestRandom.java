8cd6f0ed4749704cd8cde383eaed82a73e27f731
孙中强 sunzhongqiang <aboysun@126.com>
2016-12-29 17:05:48 +0800

    随机数测试

--- src/test/java/com/mmk/random/TestRandom.java
+++ src/test/java/com/mmk/random/TestRandom.java
@@ -69,6 +69,34 @@ public class TestRandom {
 		log.info(random.nextInt(10));
 		log.info(random.nextInt(10));
 		log.info(random.nextInt(10));
+		
+		int[] getRandomSequence2 = GetRandomSequence2(100);
+		for (int i = 0; i < getRandomSequence2.length; i++) {
+			log.info("test:"+getRandomSequence2[i]);
+		}
 	}
+	
+	
+	public static int[] GetRandomSequence2(int total)
+    {
+  	  int[] sequence = new int[total];
+        int[] output = new int[total];
+
+        for (int i = 0; i < total; i++){
+            sequence[i] = i;
+        }
+
+        Random random = new Random();
+        int end = total - 1;
+
+        for (int i = 0; i < total; i++){
+            int num = random.nextInt(total);
+            output[i] = sequence[num];
+            sequence[num] = sequence[end];
+            end--;
+        }
+
+        return output;
+    }
 
 }
